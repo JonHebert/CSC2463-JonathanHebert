ce2ccdd7f46cb470815dffafb93b9b69bfc3c5c4
JonHebert <jonah00x00@gmail.com>
2024-02-15 17:09:35 -0600

    fix

--- Assignment 3/sketch.js
+++ Assignment 3/sketch.js
@@ -18,38 +18,71 @@ function setup() {
   createCanvas(400, 400);
 }
 
+
 function draw() {
   background(0);
 
-  if (sprite.x > width) {
-    sprite.vel.x = -1;
-    sprite.scale.x = -1;
+  if (kb.pressing('d')) {
+    walkRight();
+  } else if (kb.pressing('a')) {
+    walkLeft();
+  } else if (kb.pressing('w')) {
+    walkUp();
+  } else if (kb.pressing('s')) {
+    walkDown();
+  } else {
+    stop();
+  }
+
+  if (sprite.x + sprite.width/4 > width) {
+    walkLeft();
+  } else if (sprite.x -sprite.width/4 < 0) {
+    walkRight();
   }
 }
 
-function keyTyped () {
-  switch(key) {
-    case 'd':
-      sprite.changeAni('walkRight');
+function stop() {
+  sprite.vel.y = 0;
+  sprite.vel.y = 0;
+  sprite.changeAni('stand');
+}
+
+function walkRight() {
+  sprite.changeAni('walkRight');
       sprite.vel.x= 1;
       sprite.scale.x= 1;
       sprite.vel.y= 0;
-      break;
-    case 'a':
-      sprite.changeAni('walkRight');
+}
+function walkLeft() {
+  sprite.changeAni('walkRight');
       sprite.vel.x= -1;
       sprite.scale.x= -1;
       sprite.vel.y= 0;
-      break;
-    case 'w':
-      sprite.changeAni('walkUp');
+}
+function walkUp() {
+  sprite.changeAni('walkUp');
       sprite.vel.x= 0;
       sprite.vel.y= -1;
-      break;
-    case 's':
-      sprite.changeAni('walkDown');
+}
+function walkDown() {
+  sprite.changeAni('walkDown');
       sprite.vel.x= 0;
       sprite.vel.y= 1;
+}
+
+function keyTypedOld () {
+  switch(key) {
+    case 'd':
+      walkRight();
+      break;
+    case 'a':
+      walkLeft();
+      break;
+    case 'w':
+      walkUp();
+      break;
+    case 's':
+      walkDown();
       break;
   }
 }
